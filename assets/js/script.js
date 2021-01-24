6d74ee5901c4b112dafab13a300282249a920a33
Archana Nagaraja <johndoe@example.com>
2021-01-24 08:50:58 -0800

    added code to display restaurants search results list as cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ var cuisineId;
 var locationBtn = document.getElementById("find-me");
 var options = {
     enableHighAccuracy: true,
-    timeout: 5000,
+    timeout: 10000,
     maximumAge: 0
 };
 
@@ -38,7 +38,7 @@ function getLocation()
 // Check whether browser supports Geolocation API or not
 if (navigator.geolocation) { // Supported
     // To add PositionOptions
-navigator.geolocation.getCurrentPosition(getPosition,error, options);
+navigator.geolocation.getCurrentPosition(getPosition,error,options);
 } else 
 { // Not supported
 alert("Oops! This browser does not support HTML Geolocation.");
@@ -80,6 +80,51 @@ function getData_City(coordinates){
 
 }
 
-detectCurrentLocation();
 
 
+function searchRestaurants(){
+    var searchBtnEl = document.getElementById("search-btn");
+    var userInput_searchFoodEl = document.getElementById("searchFood");
+    searchBtnEl.addEventListener("click", function(){
+        getData_Restaurants(cityId, userInput_searchFoodEl.value);
+        userInput_searchFoodEl.value = '';
+    })
+
+    function getData_Restaurants(cityId, userInput_searchFood){
+        var apiUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=' +cityId +'&entity_type=city&q=' +userInput_searchFood;
+        fetch(apiUrl, {method: "GET", 
+        headers: {
+            "user-key": zomato_api_key
+        }})
+        .then(function(response){
+            console.log(response);
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            displayrestaurantsList(data);
+        });
+    };
+}
+
+function displayrestaurantsList(data){
+    let restaurantsResponselist = data.restaurants;
+   $("#restaurants-container").empty();
+    for (let i = 0; i < restaurantsResponselist.length; i++){
+        const card = $("<div>").addClass("w3-card w3-white");
+        const cardheader_restName = $("<header>").addClass("w3-container w3-black").text(restaurantsResponselist[i].restaurant.name);
+        const restRating = $("<p>").addClass("w3-container").text(restaurantsResponselist[i].restaurant.user_rating.aggregate_rating);
+        const restRating_text = $("<p>").addClass("w3-container").text(restaurantsResponselist[i].restaurant.user_rating.rating_text);
+        const restEstablisment = $("<p>").addClass("w3-container").text(restaurantsResponselist[i].restaurant.establishment[0]);
+        const restAddress = $("<p>").addClass("w3-container").text(restaurantsResponselist[i].restaurant.location.address);
+        card.append(cardheader_restName, restRating, restRating_text ,restEstablisment,restAddress);
+        $("#restaurants-container").append(card);
+     }
+}
+
+function displayRestaurants(){
+    detectCurrentLocation();
+    searchRestaurants();
+}
+
+displayRestaurants();
