53e07e152375aaef0ebda7160c28b921d594e277
Archana Nagaraja <johndoe@example.com>
2021-01-23 22:12:08 -0800

    added detectCurrentLocation code- fixed asynchronous call issue, added displayrestaurants fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,7 @@ Get the city id from the zomato cities API using lat and long values
 Store city id in a variable
 Get the cuisine Type from the user
 Store it in a variable
-Get a list of all cuisines of restaurants listed in the city.
-Confirm the user's fav cuisineType is in the list of cuisines
-if so, get the cuisine id from the zomato cusines API
-Store it in a variable
-Now we have cusineid and city id
+Now we have cusineType and city id
 Get the restaurants lists from the zomato search api with the following inputs in the url : apikey, entity_id (city_id), entity_type( city), cuisineid
 display name, location-address, user_rating- aggregate_rating( rating_text), photos_url from the response
 
@@ -20,46 +16,102 @@ display name, location-address, user_rating- aggregate_rating( rating_text), pho
 const zomato_api_key = "5bb1aedf9a190120f2dd61a33a8368b1";
 const maps_api_key = "AIzaSyDEzBEuaOLJJZAyG4HvMvXCkRghvR0AMbU";
 var coordinates = [];
+var cityNameEl = document.getElementById("cityName");
+var cityId;
+var cityName;
+var cuisineId;
+var locationBtn = document.getElementById("find-me");
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
-$(document).ready(function(){
-    init();
+function detectCurrentLocation(){
+locationBtn.addEventListener('click', function(){
+    console.log("button clciked");
+    getLocation();
+})
 
-function init(){
-    // detect current location
-    findMe(); 
+function getLocation()
+{
+// Check whether browser supports Geolocation API or not
+if (navigator.geolocation) { // Supported
+    // To add PositionOptions
+navigator.geolocation.getCurrentPosition(getPosition,error, options);
+} else 
+{ // Not supported
+alert("Oops! This browser does not support HTML Geolocation.");
+}
 }
 
+function getPosition(position)
+{
+    coordinates.push(position.coords.latitude);
+    coordinates.push(position.coords.longitude);
+    getData_City(coordinates);
+}
 
-function findMe(){
-    $("#find-me").click(function(){
-        console.log("button clicked");
-        if(navigator.geolocation)
-            navigator.geolocation.getCurrentPosition(function(position){
-                console.log(position);
-                var lat = position.coords.latitude;
-                var lng = position.coords.longitude;
-                coordinates.push(lat);
-                coordinates.push(lng);
-                //console.log(coordinates); 
-                return coordinates;
-    });
-    else    
-        console.log("geolocation is not supported");      
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function getData_City(coordinates){
+    console.log(coordinates[0]);
+    console.log(coordinates[1]);
+    var apiUrl = 'https://developers.zomato.com/api/v2.1/cities?lat=' +coordinates[0] +'&lon=' +coordinates[1];
+    // make a request to the url
+
+    fetch(apiUrl, {method: "GET", 
+    headers: {
+        "user-key": zomato_api_key
+    }})
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        //console.log(data.location_suggestions[0].name);
+        cityName = data.location_suggestions[0].name;
+        cityNameEl.setAttribute("placeholder", cityName);
+       cityId = data.location_suggestions[0].id;
     });
 };
 
+}
+
+detectCurrentLocation();
+
+function displayRestaurants(){
+    var searchBtnEl = document.getElementById("search-btn");
+    var userInput_searchFoodEl = document.getElementById("searchFood");
+    searchBtnEl.addEventListener("click", function(){
+        getData_Restaurants(cityId, userInput_searchFoodEl.value);
+        userInput_searchFoodEl.value = '';
+    })
+
+    function getData_Restaurants(cityId, userInput_searchFood){
+        var apiUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=' +cityId +'&entity_type=city&q=' +userInput_searchFood;
+        fetch(apiUrl, {method: "GET", 
+        headers: {
+            "user-key": zomato_api_key
+        }})
+        .then(function(response){
+            console.log(response);
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            //let restaurantslist = data.restaurants;
+            for (var i=0; i<data.restaurants.length; i++){
+                console.log(data.restaurants[i].restaurant.name);
+                console.log(data.restaurants[i].restaurant.user_rating.aggregate_rating);
+                console.log(data.restaurants[i].restaurant.establishment[0]);
+                console.log(data.restaurants[i].restaurant.location.address);
+            }
+        });
+    };
+}
 
 
-
-
-
-});
-
-
-
-
-
-
-
-
-
+displayRestaurants();
